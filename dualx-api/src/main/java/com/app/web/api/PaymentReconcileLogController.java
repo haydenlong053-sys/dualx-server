f61288222d43d5564d6854a22bb82ebb2ac82b1a
Ouyang Peng <ouyangpeng58@gmail.com>
2026-08-04 15:28:47 +0800

    统一使用商城的

--- dualx-api/src/main/java/com/app/web/api/PaymentReconcileLogController.java
+++ dualx-api/src/main/java/com/app/web/api/PaymentReconcileLogController.java
@@ -39,7 +39,7 @@ public class PaymentReconcileLogController {
     @GetMapping("/{orderId}")
     @Operation(summary = "根据ID查询")
     public JSONObject findOne(@PathVariable String orderId) {
-        return paymentReconcileLogService.getWithdrawalByOrderId(orderId, withdrawContractConfig.getPaymentContractAddress());
+        return paymentReconcileLogService.getWithdrawalByOrderId(orderId, withdrawContractConfig.getPaymentProductContractAddress());
     }
 
     @PostMapping("/getPayByOrdCtr")
